79cd0f1c83ca5c26037fa6cf6c581107b9a3467a
HTML-God <dannyharkot@gmail.com>
2025-04-16 10:34:11 +0100

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -78,7 +78,7 @@
         showMessage('login is successful', 'signInMessage');
         const user=userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='homepage.html';
+        window.location.href='dashbaord.html';
     })
     .catch((error)=>{
         const errorCode=error.code;
